aa75576fe9cc0f95bce7ce89549d2699438e1971
ZL&EL <lzc123456ab@gmail.com>
2023-07-16 22:16:57 -0400

    2

---
+++ src/app/sales-person-list/sale-person.ts
@@ -0,0 +1,5 @@
+export class SalePerson {
+    constructor(public firstName: string, public lastName: string,
+        public email: string, public salesVolume: number) { 
+    }
+}
--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalePerson } from './sale-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesPersonListComponent implements OnInit {
 
+  // create an array of sale person objects
+
+  salesPersonsList : SalePerson[] = [
+    new SalePerson('Raj','Mumbai',"email@rbc.com",10000),
+    new SalePerson('fff','dfdfd',"fmail@rbc.com",10),
+    new SalePerson('wwww','qqqq',"qmail@rbc.com",440),
+  ];
   constructor() { }
 
   ngOnInit(): void {
